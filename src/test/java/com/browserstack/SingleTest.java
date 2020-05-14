234013d04acf5b076bfde5f0b5a1ae5185038ba3
Alex Li <alexliinc@gmail.com>
2020-05-13 18:56:23 -0700

    requirements
    5 browser’s
    3 tests

--- src/test/java/com/browserstack/SingleTest.java
+++ src/test/java/com/browserstack/SingleTest.java
@@ -1,5 +1,6 @@
 package com.browserstack;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -10,12 +11,25 @@ public class SingleTest extends BrowserStackJUnitTest {
 
   @Test
   public void test() throws Exception {
-    driver.get("https://www.google.com/ncr");
-    WebElement element = driver.findElement(By.name("q"));
-    element.sendKeys("BrowserStack");
-    element.submit();
-    Thread.sleep(5000);
+//    --google test--
+//    driver.get("https://www.google.com/ncr");
+//    WebElement element = driver.findElement(By.name("q"));
+//    element.sendKeys("BrowserStack");
+//    element.submit();
+//    Thread.sleep(5000);
+//
+//    assertEquals("BrowserStack - Google Search", driver.getTitle());
+    driver.get("https://www.browserstack.com/");
+    String ActualTitle = driver.getTitle();
+    String ExpectedTitle = "Most Reliable App & Cross Browser Testing Platform | BrowserStack";
+    Assert.assertEquals(ExpectedTitle, ActualTitle);
 
-    assertEquals("BrowserStack - Google Search", driver.getTitle());
+    String ActualUrl = driver.getCurrentUrl();
+    String ExpectedUrl = "https://www.browserstack.com/";
+    Assert.assertEquals(ExpectedUrl, ActualUrl);
+
+    String ActualH1 = driver.findElement(By.tagName("h1")).getText();
+    String ExpectedH1 = "App & Browser Testing Made Easy";
+    Assert.assertEquals(ExpectedH1, ActualH1);
   }
 }
